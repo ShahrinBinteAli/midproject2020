8dfac1465dd389ff8585e2fb4520dd8fb93cf214
ShahrinBinteAli <60428392+ShahrinBinteAli@users.noreply.github.com>
2020-03-09 04:49:35 +0600

    c05

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<h1>
+		Login Page 
+	</h1>
+	<form method="POST" action="logCheck.php">
+		Username: <input type="text" name="uname" value=""> <br>
+		Password: <input type="password" name="password" value=""><br>
+		<input type="submit" name="Login" value="Submit"><br>
+	</form>
+</body>
+</html>
